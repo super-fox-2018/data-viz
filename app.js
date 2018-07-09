2b7cdd18bda553fbd0524731eedc3919e4454d23
asepridwan1989 <aseprmail@gmail.com>
2018-07-09 22:17:17 +0700

    done

--- app.js
+++ app.js
@@ -1,109 +1,88 @@
-import * as d3 from 'd3'
+var margin = {top:50, right:50, bottom:50, left:50}
+var width = 600 - margin.left - margin.right
+var height = 600 - margin.top - margin.bottom
 
-// let dataset = [5, 10, 15, 20, 25]
-let dataset = []
+var svg = d3.select("#chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate("+ margin.left + "," + margin.top+")")
+
+var div = d3.select("body").append("div")	
+        .attr("class", "tooltip")				
+        .style("opacity", 0);
+
+var x = d3.scaleBand()
+          .range([0, width])
+          .padding(0.2)
+
+var y = d3.scaleLinear()
+          .range([height, 0])
+
 d3.csv("WorldCups.csv")
-.then( function(data) {
-  data.forEach(function(att){
-    dataset.push(+att.GoalsScored)
-  })
-
-  // dataset = dataset.slice(0,5)
-
-  const svg = d3.select('#chartarea').append('svg')
-  .attr('width',600)
-  .attr('height',300)
-  
-  const yScale = d3.scaleLinear()
-  .domain([0,d3.max(dataset)])
-  .range([0,300])
-
-  const colorScale = d3.scaleLinear()
-    .domain([0,d3.max(dataset)])
-    .range(['peru','teal'])
-  
-  svg.selectAll('rect')
-    .data(dataset)
-    .enter()
-    .append('rect')
-    .attr('class', 'bar')
-    .attr('x',function(d, i){
-      return i * 22
+  .then(function(data){
+    console.log(data)
+    data.forEach(function(d){
+      d.GoalsScored = + d.GoalsScored
     })
-    .attr('y', function(d){
-      return 300 - yScale(d)
+
+    const colorScale = d3.scaleLinear()
+    .domain([5, d3.max(data, function(d){
+      return d.GoalsScored
+    })])
+    .range(['blue','green'])
+
+    x.domain(data.map(function(d){
+      return d.Year
+    }))
+
+    y.domain([0, d3.max(data, function(d){
+      return d.GoalsScored
+    })])
+
+    svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr('class', 'char')
+    .attr("x", function(d) {
+      return x(d.Year)
     })
-    .attr('width',20)
-    .attr('height',function(d){
-      return yScale(d)
+    .attr("width", x.bandwidth())
+    .attr("y", function(d){
+      return y(d.GoalsScored)
     })
-    .attr('fill', colorScale)
+    .attr("height", function(d){
+      return height - y(d.GoalsScored)
+    })
+    .attr('fill', function(d){
+      return colorScale(d.GoalsScored)
+    })
     .on('mouseover', function (d, i){
       d3.select(this).style('fill', '#bada55')
+      div.transition()		
+        .duration(200)		
+        .style("opacity", .9);		
+      div	
+      .html( "year : " + d.Year + "<br/>" + "goals : " + d.GoalsScored)	
+      .style("left", (d3.event.pageX) + "px")		
+      .style("top", (d3.event.pageY - 28) + "px");	
     })
     .on('mouseout', function(d, i){
-      d3.select(this).style('fill', colorScale(d))
+      d3.select(this).style('fill', function(d){
+        div.transition()		
+        .duration(500)		
+        .style("opacity", 0);	
+        return colorScale(d.GoalsScored)
+      })
     })
 
-    svg.append("text")
-    .attr("class", "x label")
-    .attr("text-anchor", "end")
-    .attr("x", 320)
-    .attr("y", 295)
-    .text("Goal scored every worldcup")
-});
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
 
-// let dataset = d3.range(15).map(function(){
-//   return Math.random()*50
-// })
-// const multiplier = 10
+    svg.append("g")
+      .call(d3.axisLeft(y))
 
+    
+  })
 
-// d3.select('body').selectAll('div')
-//   .data(dataset)
-//   .enter()
-//   .append('div')
-//   .attr('class','bar')
-//   .style('height', function(d){
-//     return d * 5 + 'px'
-//   })
-
-
-
-
-
-// svg.selectAll('rect')
-//   .data(dataset)
-//   .enter()
-//   .append('rect')
-//   .attr('class','bar')
-//   .attr('x', function(d, i){
-//     return i * 22
-//   })
-//   .attr('y', function(d){
-//     return 300 - d * multiplier
-//   })
-//   .attr('width',20)
-//   .attr('height', function(d){
-//     return d * multiplier
-//   })
-
-
-
-// svg.selectAll('rect')
-//   .data(dataset)
-//   .enter()
-//   .append('rect')
-//   .attr('class', 'bar')
-//   .attr('x', function(d, i){
-//     return i
-//   })
-//   .attr('y', function(d){
-//     return 300 - yScale(d)
-//   })
-//   .attr('width',20)
-//   .attr('height',function(d){
-//     return yScale(d)
-//   })
-
-  
